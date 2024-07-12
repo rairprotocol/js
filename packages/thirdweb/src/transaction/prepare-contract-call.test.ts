820e2921622528dc7b53221d48cd9a9f3bf3cd75
Kien Ngo <github@kien.ngo>
2024-07-12 15:01:24 -0700

    Add unit test

--- packages/thirdweb/src/transaction/prepare-contract-call.test.ts
+++ packages/thirdweb/src/transaction/prepare-contract-call.test.ts
@@ -5,6 +5,7 @@ import { TEST_WALLET_B } from "../../test/src/addresses.js";
 import { USDT_CONTRACT_WITH_ABI } from "../../test/src/test-contracts.js";
 import { toWei } from "../utils/units.js";
 import { prepareContractCall } from "./prepare-contract-call.js";
+import { fromHex } from "../utils/encoding/hex.js";
 
 describe("prepareContractCall", () => {
   test("should prepare a contract call with ABI", () => {
@@ -14,5 +15,21 @@ describe("prepareContractCall", () => {
       params: [TEST_WALLET_B, toWei("100")],
     });
     expect(preparedTx.to).toBe(USDT_CONTRACT_WITH_ABI.address);
+  });
+
+  test("should be able to prepare a contract call with extra call data", async () => {
+    const extraCallData = "I'm a cat";
+    const preparedTx = prepareContractCall({
+      contract: USDT_CONTRACT_WITH_ABI,
+      method: "transfer",
+      params: [TEST_WALLET_B, toWei("100")],
+      extraCallData,
+    });
+    const callData =
+      typeof preparedTx.data === "string"
+        ? preparedTx.data
+        : (await preparedTx.data?.()) || "0x";
+    const decodedData = fromHex(callData, "string");
+    expect(decodedData).toContain(extraCallData);
   });
 });
